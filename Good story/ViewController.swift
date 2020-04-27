0737e274c7aeff1fda20116e847b8007b2fb96d2
Jan <jan.barta@hotmail.cz>
2020-04-27 09:19:08 -0700

    Initial Commit

---
+++ Good story/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Good story
+//
+//  Created by Jan B on 27/04/2020.
+//  Copyright © 2020 broflowski. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
